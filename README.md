1e4593ae2ba4ee2db2c827efa44b7bde6433565f
João Taveira Araújo <joao@observeinc.com>
2021-12-08 14:18:25 -0800

    feat: first commit

---
+++ README.md
@@ -0,0 +1,82 @@
+# Observe kustomize manifests
+
+This repository contains kustomized Kubernetes manifests supported by Observe.
+These are intended as a way of quickly ingesting Kubernetes data into your
+Observe instance.
+
+# Quick setup
+
+Try it out by installing our kustomized stack:
+
+```
+kubectl apply -k github.com/observeinc/manifests/stack
+kubectl -n observe create secret generic credentials \
+        --from-literal=customer=${OBSERVE_CUSTOMER?} \
+        --from-literal=token=${OBSERVE_TOKEN?}
+```
+
+This will create an `observe` namespace for you and start collecting Kubernetes state, logs and metrics.
+
+# Components
+
+We provide four "base" components:
+
+- `bases/events` - responsible for collecting Kubernetes state using `kube-state-events`
+- `bases/logs` - responsible for collecting container logs using `fluent-bit`
+- `bases/metrics` - responsible for collecting container logs using `grafana-agent`
+- `bases/traces` - responsible for collecting traces using `otel-collector`
+
+We compose these base layers into overlays. Our default `stack` overlay
+collects events, logs and metrics.
+
+# Sizing
+
+By default, we attempt to choose defaults which have a wide operating
+range. However, some clusters will inevitably fall outside this range. We
+provide additional configurations that are more appropriate for these extremes:
+
+- `github.com/observeinc/stack/xs` - intended to run on small clusters such as development environments, where resources are scarce and reliability is less of a concern
+- `github.com/observeinc/stack/xl` - intended to run on large clusters with 100+ nodes
+
+# Configuration
+
+You can override any individual configuration element by creating a new
+kustomized manifest with our kustomized directory as a base.
+
+The following example creates a new directory with `kustomization.yaml` set to
+override the `FB_DEBUG` variable in the fluent-bit environment variable
+configmap:
+
+```
+EXAMPLE_DIR=$(mktemp -d)
+
+cat <<EOF >$EXAMPLE_DIR/kustomization.yaml
+bases:
+  - github.com/observeinc/manifests/stack?ref=main
+
+configMapGenerator:
+  - name: fluent-bit-env
+    behavior: merge
+    literals:
+      - FB_DEBUG=true
+EOF
+
+kubectl apply -k $EXAMPLE_DIR
+```
+
+You can version control your kustomized directory while tracking upstream changes through the use of branch tags.
+
+# Pruning and deletion
+
+All Kubernetes resources installed through this repo will have an
+`observeinc.com/component` label. You can therefore use this label for pruning an existing install:
+
+```
+kubectl apply -k github.com/observeinc/manifests/stack --prune -l observeinc.com/component
+```
+
+To delete an existing install, just use `delete -k`:
+
+```
+kubectl delete -k github.com/observeinc/manifests/stack
+```
---
+++ scripts/test.sh
@@ -0,0 +1,128 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+
+DIE() { echo "${R}E! $*${r}" 1>&2; exit 1; }
+INFO() { echo "${B}I: $*${r}" 1>&2; }
+DEBUG() { test -z "$VERBOSE" || echo "D: $*" 1>&2; }
+
+R="[1;31m"
+B="[1;34m"
+r="[m"
+
+KUSTOMIZE_DIR="${KUSTOMIZE_DIR:=stack}"
+CLUSTER_NAME="${CLUSTER_NAME:=unknown}"
+WAIT_TIMEOUT="${WAIT_TIMEOUT:=30s}"
+
+usage() {
+    echo "${B}$0 [-hv] <command>${r}"
+    echo "  -h     this help"
+    echo "  -v     verbose"
+    echo
+    echo "  ${B}apply${r}       Apply kustomized manifest"
+    echo "  ${B}delete${r}      Delete kustomized manifest"
+    echo "  ${B}label_check${r} Verify all resources contain correct label"
+    echo
+    exit 1
+}
+
+check_context() {
+    if [[ `kubectl config current-context` != kind* ]]; 
+    then
+        DIE "Context must be created by kind"
+    fi;
+}
+
+do_apply() {
+    check_context
+
+    DEBUG "$(kubectl cluster-info)"
+
+    INFO "Applying kustomized directory"
+    RESULT=$(kubectl apply -k ${KUSTOMIZE_DIR})
+    DEBUG "$RESULT"
+
+    INFO "Annotating namespace"
+    RESULT=$(kubectl annotate namespace observe --overwrite "observeinc.com/cluster-name=${CLUSTER_NAME}")
+    DEBUG "$RESULT"
+
+    INFO "Creating secret"
+    # this is an arcane way of doing things, but it allows overwriting existing secret without
+    # relying on kubectl --dry-run semantics, which is version dependent
+    RESULT=$(kubectl apply -f - << EOF
+    {
+        "kind": "Secret",
+        "apiVersion": "v1",
+        "metadata": {
+            "name": "credentials",
+            "namespace": "observe",
+            "annotations": {
+                "kubectl.kubernetes.io/last-applied-configuration": ""
+            }
+        },
+        "data": {
+            "customer": "$(echo -n $OBSERVE_CUSTOMER | base64)",
+            "token": "$(echo -n $OBSERVE_TOKEN | base64)"
+        }
+    }
+EOF
+)
+    DEBUG "$RESULT"
+
+    INFO "Waiting on pods to be ready"
+    if kubectl wait pods -n observe --for=condition=Ready --all --timeout=${WAIT_TIMEOUT}; then
+        INFO "Pods ready"
+        return
+    else
+        diagnostics
+        DIE "Pod readiness timed out"
+    fi
+}
+
+do_label_check() {
+    KUSTOMIZED=`find . -name 'kustomization.yaml' -exec dirname {} ';'`
+    for DIR in $KUSTOMIZED
+    do
+        rc=0
+        RESULT=$(kubectl apply -k ${DIR} --dry-run=client -o yaml -l \!observeinc.com/component 2>&1) || rc=$?
+        if [ "$rc" == 0 ]; then
+            DEBUG "$RESULT"
+            DIE "Resources missing component label in ${DIR}"
+        else
+            DEBUG "${DIR} labels check out"
+        fi
+    done
+    return
+}
+
+do_delete() {
+    check_context
+
+    if kubectl delete -k ${KUSTOMIZE_DIR} --wait --timeout=${WAIT_TIMEOUT}; then
+        return
+    else
+        diagnostics
+        DIE "Pod deletion timed out"
+    fi
+}
+
+diagnostics() {
+    kubectl version
+    kubectl get pods -n observe
+    kubectl describe pods -n observe
+}
+
+while getopts 'hv' OPTION; do
+    case $OPTION in
+        v) VERBOSE=1 ;;
+        *) usage ;;
+    esac
+done
+shift $((OPTIND-1)); OPTIND=1
+[[ -n "$VERBOSE" ]] && export VERBOSE
+
+if [[ $# -lt 1 ]]; then usage; fi # Requires 1+ arguments
+COMMAND="$1"
+shift
+
+"do_${COMMAND//-/_}" "$@"
